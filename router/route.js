aaeab98680f4fb9f5e6766d86f20638bdf227160
M.Abdullah Alvi <abdullah.alvi22@gmail.com>
2023-01-26 00:27:45 +0500

    inital commit

---
+++ router/route.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const { ownerImages, changeOwner, singleproduct, verifySignup, Specificsubcategory, Specificcategory, signIn, allrole, addrole, deleterole, updaterole, allproduct, addproduct, deleteproduct, updateproduct, allcategory, addcategory, deletecategory, updatecategory } = require('../controller/demo')
+const { checkMissingField, checkDuplicateEmail } = require("../middleware/sign.validate");
+const verifyToken = require("../middleware/auth")
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:|\./g, '') + ' - ' + file.originalname)
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+//upload name k folder may files save hogi
+const upload = multer({ storage: storage, fileFilter: fileFilter })
+
+//Get All Product
+router.get("/api/allproduct", allproduct);
+//Change Owner
+router.post("/api/changeOwner", [verifyToken], changeOwner);
+//Get Single Product
+router.get("/api/singleproduct", singleproduct);
+//Get Single Product
+router.get("/api/ownerImages", [verifyToken], ownerImages);
+//Insert A Product
+router.post("/api/addproduct", [verifyToken, upload.single('productImage')], addproduct);
+//Delete A Product
+router.delete("/api/deleteproduct", [verifyToken], deleteproduct);
+//Update A Product
+router.put("/api/updateproduct", [verifyToken], updateproduct);
+
+
+//Get All Category
+router.get("/api/allcategory", allcategory);
+//Get Specific Category
+router.get("/api/Specificcategory", [verifyToken], Specificcategory);
+//Get Specific Sub Category
+router.get("/api/Specificsubcategory", [verifyToken], Specificsubcategory);
+//Insert A category
+router.post("/api/addcategory", [verifyToken], addcategory);
+//Delete A category
+router.delete("/api/deletecategory", [verifyToken], deletecategory);
+//Update A category
+router.put("/api/updatecategory", [verifyToken], updatecategory);
+
+
+//Get All role
+router.get("/api/allrole", [verifyToken], allrole);
+//Insert A role
+router.post("/api/addrole", [verifyToken], addrole);
+//Delete A role
+router.delete("/api/deleterole", [verifyToken], deleterole);
+//Update A role
+router.put("/api/updaterole", [verifyToken], updaterole);
+
+//signup
+router.post("/api/Signup", [checkDuplicateEmail, checkMissingField], verifySignup);
+//login
+router.post('/api/signIn', signIn)
+
+module.exports = router
